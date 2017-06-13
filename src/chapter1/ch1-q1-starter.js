15843a9ec28090d792ab3a984e596df7b726df13
d-kang <david.kang714@gmail.com>
2017-06-13 14:10:36 -0700

    Implement 1.1

---
+++ src/chapter1/ch1-q1-starter.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+
+/**
+* Is Unique: Implement an algorithm to determine if a string has all
+* unique characters. What if you cannot use additional data structures?
+*/
+
+
+
+
+// function hasUniqueCharactersSet(str) {
+//
+// }
+
+
+
+
+
+// function hasUniqueCharactersSort(str) {
+//
+// }
+
+
+
+
+/**
+* What is the Time Complexity?
+*
+* What is the Space Complexity?
+*/
--- src/chapter1/ch1-q1.js
+++ src/chapter1/ch1-q1.js
@@ -1,46 +1,43 @@
 'use strict';
 
+
+
 /**
- * Keep track of seen characters with a Set data structure, fail when
- * a repeated character is found.
- *
- * Time: O(N)
- * Additional space: O(N)
- *
- * @param  {string[]} str String to check, passed in as a character array
- * @return {boolean}      True if unique characters, otherwise false
- */
+* Is Unique: Implement an algorithm to determine if a string has all
+* unique characters. What if you cannot use additional data structures?
+*/
+
+
+
+
 export function hasUniqueCharactersSet(str) {
-  let chars = new Set();
-
-  for (let i = 0; i < str.length; ++i) {
-    if (chars.has(str[i])) {
+  console.log('typeof str:', typeof str)
+  console.log('Array.isArray(str):',Array.isArray(str))
+  let strList = str.split('');
+  return strList.reduce((prev, next, i) => {
+    if (!prev) {
       return false;
     }
-    chars.add(str[i]);
-  }
-  return true;
+    return strList.indexOf(next, i + 1) === - 1;
+  }, true);
 }
+
+console.log('hasUniqueCharactersSet(\'hello\')', hasUniqueCharactersSet('hello'));
+console.log('hasUniqueCharactersSet(\'hi\')', hasUniqueCharactersSet('hi'));
+
+
+
+
+
+// export function hasUniqueCharactersSort(str) {
+//
+// }
+
+
+
 
 /**
- * Sort the original string first then iterate through it. Repeat characters
- * will show up next to eachother so fail if any two characters in a row
- * are the same.
- *
- * Time: O(N lg N)
- * Additional space: O(1)
- *
- * @param  {string[]} str String to check, passed in as a character array
- * @return {boolean}      True if unique characters, otherwise false
- */
-export function hasUniqueCharactersSort(str) {
-  // sort string using quicksort
-  str.sort();
-
-  for (var i = 1; i < str.length; ++i) {
-    if (str[i] === str[i - 1]) {
-      return false;
-    }
-  }
-  return true;
-}
+* What is the Time Complexity?
+*
+* What is the Space Complexity?
+*/
